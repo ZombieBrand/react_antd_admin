257ccfbe3773418367ccaf6ff04a7bf9b1221913
ZombieBrand <zombiebrandg@gmail.com>
2023-05-19 16:19:27 +0800

    add eslint,prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,22 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'no-console': 'warn', // 禁止使用console
+        'no-unused-vars': 'error', // 禁止定义未使用的变量
+        'no-debugger': 'error', // 禁止使用debugger
+        'no-var': 'error' // 要求使用 let 或 const 而不是 var
+    }
 }
