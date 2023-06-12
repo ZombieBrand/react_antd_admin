3baf58617afe98fa3a459447411d6fecf2e153de
ZombieBrand <zombiebrandg@gmail.com>
2023-06-12 17:38:00 +0800

    feat: 首页登录样式开发完成

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import router from '@/router/index'
 import './App.css'
 import { ConfigProvider, App as AppProvider, theme } from 'antd'
 import { useDarkMode } from '@/hook'
+import zhCN from 'antd/locale/zh_CN'
+
 function App() {
     const isDarkMode = useDarkMode()
     return (
-        <ConfigProvider theme={{ algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm }}>
+        <ConfigProvider theme={{ algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm }} locale={zhCN}>
             <AppProvider>
                 <RouterProvider router={router} />
             </AppProvider>
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,50 +1,33 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { LoginForm, ProFormText, ProConfigProvider } from '@ant-design/pro-components'
+import { Button, Card, Form, Input } from 'antd'
 import { useDarkMode } from '@/hook'
+import './index.less'
+
 const Login = () => {
     const isDarkMode = useDarkMode()
     const backgroundColor = isDarkMode ? '#252525' : 'white'
+    const onFinish = (values: any) => {
+        console.log('Received values of form: ', values)
+    }
     return (
-        <ProConfigProvider hashed={false}>
-            <div style={{ backgroundColor }}>
-                <LoginForm
-                    logo='https://github.githubassets.com/images/modules/logos_page/Octocat.png'
-                    title='Github'
-                    subTitle='全球最大的代码托管平台'
-                >
-                    <>
-                        <ProFormText
-                            name='username'
-                            fieldProps={{
-                                size: 'large',
-                                prefix: <UserOutlined className={'prefixIcon'} />
-                            }}
-                            placeholder={'用户名: admin or user'}
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '请输入用户名!'
-                                }
-                            ]}
-                        />
-                        <ProFormText.Password
-                            name='password'
-                            fieldProps={{
-                                size: 'large',
-                                prefix: <LockOutlined className={'prefixIcon'} />
-                            }}
-                            placeholder={'密码: ant.design'}
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '请输入密码！'
-                                }
-                            ]}
-                        />
-                    </>
-                </LoginForm>
-            </div>
-        </ProConfigProvider>
+        <div className='login' style={{ backgroundColor }}>
+            <Card className='login-wrapper'>
+                <h2 className='login-title'>Ant Design Admin</h2>
+                <Form name='login' initialValues={{ remember: true }} onFinish={onFinish} autoComplete='off'>
+                    <Form.Item name='username' rules={[{ required: true, message: '请输入您的用户名' }]}>
+                        <Input prefix={<UserOutlined className='site-form-item-icon' />} />
+                    </Form.Item>
+                    <Form.Item name='password' rules={[{ required: true, message: '请输入您的密码!' }]}>
+                        <Input prefix={<LockOutlined className='site-form-item-icon' />} type='password' />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button size='large' style={{ width: '100%' }} type='primary' htmlType='submit'>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
     )
 }
 export default Login
